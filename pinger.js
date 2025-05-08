1a0cdcb2cf876db59fe7edec1ebc166ed2f61f10
vipulattri <attrivipul72@gmail.com>
2025-05-08 16:07:36 +0530

    Initial pinger script

---
+++ pinger.js
@@ -0,0 +1,26 @@
+// pinger.js
+
+const https = require('https');
+const http = require('http');
+
+const url = 'https://vipul-attri-portfolio.netlify.app/'; // <-- Replace with your website
+
+function pingWebsite() {
+  const lib = url.startsWith('https') ? https : http;
+  
+  const req = lib.get(url, (res) => {
+    console.log(`[${new Date().toLocaleTimeString()}] Pinged ${url} - Status Code: ${res.statusCode}`);
+  });
+
+  req.on('error', (err) => {
+    console.error(`[${new Date().toLocaleTimeString()}] Error pinging ${url}:`, err.message);
+  });
+
+  req.end();
+}
+
+// Ping immediately
+pingWebsite();
+
+// Then ping every 30 seconds
+setInterval(pingWebsite, 30000);
